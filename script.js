4554970b5f046c642b7d8c023a9a5d619989cf29
aneyeopen <daniel.martin603@icloud.com>
2023-01-20 17:04:24 +1100

    bare bones html and styling so far

---
+++ script.js
@@ -0,0 +1,29 @@
+const gameBoard (() => {
+    
+    const playerTemplate = (name, symbol, playerArray, ai, turn) => {
+        return { name, symbol, playerArray, ai, turn}
+    }
+
+    const playerOne = playerTemplate('player one', 'X', [], false, true);
+    const playerTwo = playerTemplate('player two', 'O', [], false, false);
+
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    let winner = null;
+
+    let turns = 0;
+
+    let board = [];
+
+
+}
+)
