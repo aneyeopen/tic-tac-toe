77df1ef7b0e8f8418805ec4c93747c6ecdc9318b
aneyeopen <daniel.martin603@icloud.com>
2023-01-21 21:30:26 +1100

    finished scripting of game, ends after win. Need to implement more UI features but I already know I can do that

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard (() => {
+const gameBoard  = (() => {
     
     const playerTemplate = (name, symbol, playerArray, ai, turn) => {
         return { name, symbol, playerArray, ai, turn}
@@ -8,14 +8,14 @@ const gameBoard (() => {
     const playerTwo = playerTemplate('player two', 'O', [], false, false);
 
     const winConditions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        ['0','1','2'],
+        ['3','4','5'],
+        ['6','7','8'],
+        ['0','3','6'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['0','4','8'],
+        ['2','4','6']
     ];
 
     let winner = null;
@@ -24,6 +24,61 @@ const gameBoard (() => {
 
     let board = [];
 
+    function checkTurn(playerOne, playerTwo) {
+        if (playerOne.turn === true) {
+        return playerOne;
+        } else {
+            return playerTwo;
+        }
+    }
 
-}
-)
+    function switchTurn(playerOne, playerTwo) {
+        if (playerOne.turn === true){
+            playerOne.turn = false;
+            playerTwo.turn = true;
+        }else {
+            playerOne.turn = true;
+            playerTwo.turn = false;
+        } return playerOne, playerTwo
+    }
+
+    
+    function checkWin(winConditions, player){
+        for (i = 0; i < winConditions.length; i += 1 ) {
+            if (winConditions[i].every(r => player.playerArray.includes(r)) === true) {
+                return true 
+            };
+        } return false;
+    }
+
+    const playerTurn = (function () {
+        
+        const boardSpace = document.querySelectorAll('.board-space');
+        boardSpace.forEach( boardSpace => {
+            boardSpace.addEventListener('click', e => {
+                let player = checkTurn(playerOne, playerTwo);
+                
+                if (e.target.textContent == '' && gameBoard.winner == null){
+                    board.push(e.target.id);
+                    player.playerArray.push(e.target.id);
+                    e.target.textContent = player.symbol;
+                    console.log(player.playerArray);
+                    console.log(board);
+                    if (checkWin(winConditions, player) === false) {
+                        switchTurn(playerOne, playerTwo);
+                    } else {
+                        document.querySelector('.page-ui').textContent = `${player.name} just won.`;
+                        gameBoard.winner = player.name;
+                    }
+                }
+            }
+            )
+    })
+                
+        return { boardSpace };
+
+
+    })();
+
+    return {checkWin, checkTurn, playerTurn, board, playerTwo, winner}
+})();
